8f12c1411400edc65dded840658279daca0c31a1
Aarsh Makwana <59681126+aarsh411@users.noreply.github.com>
2020-10-27 18:16:27 +0530

    basic insert query
    insert query and database connectivity

---
+++ maindb.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	
+	db, err := sql.Open("mysql", "aarsh:1234@(127.0.0.1:3306)/kloudonedata")
+	fmt.Println(db)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+	//insert, err := db.Query("INSERT INTO users VALUES ( 'Divyang' )")
+	insert, err := db.Query("INSERT INTO golang VALUES ( 2,'Dhiraj','2020-09-28'),(3,'divyang','2020-09-28')")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer insert.Close()
+}
